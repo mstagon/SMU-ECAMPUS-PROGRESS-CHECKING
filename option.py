82145e7753c6ac1db33d925006addf1a6630e1ef
cms <83182868+mstagon@users.noreply.github.com>
2021-11-30 19:56:12 +0900

    Add files via upload

---
+++ option.py
@@ -0,0 +1,13 @@
+#[필수설정]
+# 과목 ID 입력란 (사용법.pdf를 참고하세요)
+# example
+# a = ['12345','12345']
+a = ['']
+
+# [선택설정]
+# 자동로그인을 원하시면 autologin = True로 바꿔주시고 각각 학번과 비밀번호를 입력해주세요.(개인정보 주의!!!)
+autologin = False #Default False (True of False)
+cn = ""  # 학번
+pw = ""  # 비밀번호
+
+
